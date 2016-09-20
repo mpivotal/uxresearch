113fac9f4c571c24b2c71103b5ee4dccd010c6b5
Mike Long <mlong@pivotal.io>
2016-09-20 16:56:41 -0700

    second commit

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,15 +1,17 @@
 class ParticipantsController < ApplicationController
   before_action :set_participant, only: [:show, :edit, :update, :destroy]
+  before_action :set_message, only: [:new, :edit, :create, :update]
 
   # GET /participants
   # GET /participants.json
   def index
-    @participants = Participant.all
+    redirect_to 'participants#new'
   end
 
   # GET /participants/1
   # GET /participants/1.json
   def show
+    redirect_to '/participants/new'
   end
 
   # GET /participants/new
@@ -28,11 +30,12 @@ class ParticipantsController < ApplicationController
 
     respond_to do |format|
       if @participant.save
-        format.html { redirect_to @participant, notice: 'Participant was successfully created.' }
-        format.json { render :show, status: :created, location: @participant }
+        # format.html { redirect_to @participant, notice: 'Participant was successfully created.' }
+        format.html { redirect_to '/confirmation/thankyou'}
+        # format.json { render :show, status: :created, location: @participant }
       else
         format.html { render :new }
-        format.json { render json: @participant.errors, status: :unprocessable_entity }
+        # format.json { render json: @participant.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -43,10 +46,10 @@ class ParticipantsController < ApplicationController
     respond_to do |format|
       if @participant.update(participant_params)
         format.html { redirect_to @participant, notice: 'Participant was successfully updated.' }
-        format.json { render :show, status: :ok, location: @participant }
+        # format.json { render :show, status: :ok, location: @participant }
       else
         format.html { render :edit }
-        format.json { render json: @participant.errors, status: :unprocessable_entity }
+        # format.json { render json: @participant.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -54,17 +57,25 @@ class ParticipantsController < ApplicationController
   # DELETE /participants/1
   # DELETE /participants/1.json
   def destroy
-    @participant.destroy
-    respond_to do |format|
-      format.html { redirect_to participants_url, notice: 'Participant was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    # @participant.destroy
+    # respond_to do |format|
+    #   format.html { redirect_to participants_url, notice: 'Participant was successfully destroyed.' }
+    #   format.json { head :no_content }
+    # end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_participant
       @participant = Participant.find(params[:id])
+    end
+
+    def set_message
+      messages = Participant::MESSAGES
+      if params[:message_id] then @message = messages[ params[:message_id].to_i] end
+      if !params[:message_id] then @message = messages.sample end
+      @headline = @message[0]
+      @subhead = @message[1]
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,2 +1,8 @@
 class Participant < ApplicationRecord
+  validates :email, presence: true
+  MESSAGES = [
+    ["Help us fix what’s broken", "...and improve what's not optimal."],
+    ["You know a lot.", "we want to learn from you"], 
+    ["We need you!", "to help us build great products"]
+  ]
 end
